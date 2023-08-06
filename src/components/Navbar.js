b06bfac505078a62399812dcf59785b56a4e06ac
Utsav Talati <115746938+utsavlambda@users.noreply.github.com>
2023-08-06 15:24:16 -0500

    Update Navbar.js
    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,31 +4,30 @@ import React from "react";
 export default function Navbar() {
   return (
     <header className="bg-gradient-to-r from-black via-gold to-black md:sticky top-0 z-10">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl text-gold">
-            Utsav 
-          </a>
-        </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-gold">
-            Past Work 
-          </a>
-          <a href="#skills" className="mr-5 hover:text-gold">
-            Skills 
-          </a>
-          <a href="#testimonials" className="mr-5 hover:text-gold">
-            Testimonials 
-          </a>
-        </nav>
-        <div className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a
-            href="#contact"
-            className="inline-flex items-center bg-gold border-2 border-black py-2 px-6 focus:outline-none hover:bg-black hover:text-white rounded-full text-base font-semibold transition-colors duration-300">
-            Hire Me
-            <ArrowRightIcon className="w-4 h-4 ml-2" />
+      <div className="container mx-auto flex justify-between items-center p-5">
+        <div>
+          <a href="#about" className="text-3xl font-bold text-white hover:text-gold">
+            Utsav Talati
           </a>
         </div>
+        <nav className="hidden md:flex space-x-5">
+          <a href="#projects" className="text-white hover:text-gold">
+            Past Work
+          </a>
+          <a href="#skills" className="text-white hover:text-gold">
+            Skills
+          </a>
+          <a href="#testimonials" className="text-white hover:text-gold">
+            Testimonials
+          </a>
+        </nav>
+        <a
+          href="#contact"
+          className="inline-flex items-center bg-gold border-2 border-black py-2 px-6 focus:outline-none hover:bg-black hover:text-white rounded-full text-base font-semibold transition-colors duration-300"
+        >
+          Hire Me
+          <ArrowRightIcon className="w-4 h-4 ml-2" />
+        </a>
       </div>
     </header>
   );
