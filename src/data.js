44f896256e6fa284715fe8d72822a9cdce9ec0ce
Utsav Talati <utsav.talati000@gmail.com>
2023-08-06 13:41:46 -0500

    initialize repository

---
+++ src/data.js
@@ -0,0 +1,55 @@
+// src/data.js
+
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reedbarger.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://pythonbootcamp.com",
+  },
+];
+
+// Skills data
+export const skills = [
+  'React', 'JavaScript', 'HTML', 'CSS', 'Node.js', 'Express.js', // Add more skills as needed
+];
+
+export const testimonials = [
+  {
+    name: 'John Doe',
+    position: 'Software Engineer',
+    testimonial: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac ligula auctor, sodales turpis eu, venenatis mi. Nullam consectetur felis sit amet lorem placerat vehicula.',
+  },
+  {
+    name: 'Jane Smith',
+    position: 'Web Developer',
+    testimonial: 'In id arcu id sapien vestibulum consectetur vitae at velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla facilisi. Vivamus tincidunt, metus vitae sagittis varius, mauris nunc bibendum velit, ac cursus quam felis ut elit.',
+  },
+  // Add more testimonials as needed
+];
